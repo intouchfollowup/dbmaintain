d11f945b8727f8250510232aeab0bc7d9ca4ba8f
Collin Peters <cpeters@intouchfollowup.com>
2012-06-07 23:12:55 -0700

    DBM-75 - Postgers enum types are not auto-dropped

--- dbmaintain/src/main/java/org/dbmaintain/database/impl/PostgreSqlDatabase.java
+++ dbmaintain/src/main/java/org/dbmaintain/database/impl/PostgreSqlDatabase.java
@@ -154,7 +154,14 @@ public class PostgreSqlDatabase extends Database {
      */
     @Override
     public Set<String> getTypeNames(String schemaName) {
-        return getSQLHandler().getItemsAsStringSet("select object_name from information_schema.data_type_privileges where object_type = 'USER-DEFINED TYPE' and object_schema = '" + schemaName + "'", getDataSource());
+        String sql = 
+        	"SELECT 	t.typname as type " +
+	        "FROM 		pg_type t " +
+	        "LEFT JOIN 	pg_catalog.pg_namespace n ON n.oid = t.typnamespace " +
+	        "WHERE 		(t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid)) " +
+	        "AND 		NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid) " +
+	        "AND 		n.nspname = '" + schemaName + "'";
+        return getSQLHandler().getItemsAsStringSet(sql, getDataSource());
     }
 
 
